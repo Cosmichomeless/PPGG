82ce7c5637bcfdf20f242d55a1112848af34c4aa
Cosmichomeless <Drodriguez1703@gmail.com>
2024-01-29 11:52:38 +0100

    nuevo

---
+++ Java/U7_CadenasCaracteres/a00.java
@@ -0,0 +1,17 @@
+package Java.U7_CadenasCaracteres;
+
+public class a00 {
+    public static void main(String[] args) {
+        String cad = "En un lugar de la mancha";
+        // cad = cad.replace('n', 'l');
+        // System.out.println(cad);
+
+        String[] palabras = cad.split(" ");
+        for (int i = 0; i < palabras.length; i++) {
+            System.out.println(palabras[i]);
+
+        }
+
+    }
+
+}
