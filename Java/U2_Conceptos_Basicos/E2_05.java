b4105a537093b4db4a0df16eaf2d41b4bb04c962
Cosmichomeless <Drodriguez1703@gmail.com>
2024-02-08 22:17:07 +0100

    caa

--- Java/U2_Conceptos_Basicos/E2_05.java
+++ Java/U2_Conceptos_Basicos/E2_05.java
@@ -1,13 +1,12 @@
 package Java.U2_Conceptos_Basicos;
 
-
 public class E2_05 {
 	public static void main(String[] args) {
 		short num;
 		num = 32767;
 		System.out.println("Valor maximo para el tipo short: " + num);
 		num++;
-		System.out.println("Valor minimo para el tipo short: " + num);es to es afa
+		System.out.println("Valor minimo para el tipo short: " + num);
 	}
 
 }
