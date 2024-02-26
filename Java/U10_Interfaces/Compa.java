63dc03d41c21e4ea8e7ec3dc48995eed8deb3bf7
cosmichomeless <Drodriguez1703@gmail.com>
2024-02-26 13:07:30 +0100

    comp

---
+++ Java/U10_Interfaces/Compa.java
@@ -0,0 +1,68 @@
+package Java.U10_Interfaces;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@SuppressWarnings("unused")
+public class Compa {
+    public static void main(String[] args) {
+
+        Persona p = new Persona();
+        p.nombre = "Amanda";
+        p.id = 3;
+
+        Persona p2 = new Persona();
+        p2.nombre = "Felipe";
+        p2.id = 1;
+
+        Persona p3 = new Persona();
+        p3.nombre = "Jesus";
+        p3.id = 2;
+
+        ArrayList<Persona> lista = new ArrayList<Persona>();
+
+        lista.add(p);
+        lista.add(p2);
+        lista.add(p3);
+
+        System.out.println(lista);
+        lista.sort(null);
+        System.out.println(lista);
+
+        // Persona[] tabla = new Persona[3];
+
+        // tabla[0] = p;
+        // tabla[1] = p2;
+        // tabla[2] = p3;
+
+        // System.out.println(Arrays.toString(tabla));
+        // Arrays.sort(tabla);
+        // System.out.println(Arrays.toString(tabla));
+    }
+
+}
+
+@SuppressWarnings("rawtypes")
+class Persona implements Comparable {
+    String nombre;
+    int id;
+
+    public String toString() {
+        return "Nombre: " + this.nombre + " id: " + this.id;
+    }
+
+    public int compareTo(Object o) {
+        Persona p = (Persona) o;
+        /*
+         * if (this.id < p.id) {
+         * return -1;
+         * } else {
+         * if (this.id > p.id) {
+         * return 1;
+         * }
+         */
+
+        // return this.nombre.compareTo(p.nombre);
+        return p.id - this.id;
+    }
+}
