5984cac2a2cd31785dea1619eb9813499f36e01d
Cosmichomeless <136361455+Cosmichomeless@users.noreply.github.com>
2024-02-26 11:31:42 +0100

    Add files via upload

---
+++ Java/Interfaces.java
@@ -0,0 +1,17 @@
+package Java.U10_Interfaces;
+
+public class Interfaces {
+    public static void main(String[] args) {
+
+    }
+}
+
+interface Sonido {
+
+}
+
+class Nombre {
+
+    String nombre;
+
+}
