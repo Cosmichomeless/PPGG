d5e184449fdd45f168b62133cfb622c632d9793e
cosmichomeless <Drodriguez1703@gmail.com>
2024-02-27 12:26:15 +0100

    comp

--- Java/U10_Interfaces/Compa.java
+++ Java/U10_Interfaces/Compa.java
@@ -2,9 +2,11 @@ package Java.U10_Interfaces;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 @SuppressWarnings("unused")
 public class Compa {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
 
         Persona p = new Persona();
@@ -19,25 +21,33 @@ public class Compa {
         p3.nombre = "Jesus";
         p3.id = 2;
 
-        ArrayList<Persona> lista = new ArrayList<Persona>();
+        // ArrayList<Persona> lista = new ArrayList<Persona>();
 
-        lista.add(p);
-        lista.add(p2);
-        lista.add(p3);
+        // lista.add(p);
+        // lista.add(p2);
+        // lista.add(p3);
 
-        System.out.println(lista);
-        lista.sort(null);
-        System.out.println(lista);
+        // System.out.println(lista);
+        // lista.sort(null);
+        // System.out.println(lista);
 
-        // Persona[] tabla = new Persona[3];
+        Persona[] tabla = new Persona[3];
+        tabla[0] = p;
+        tabla[1] = p2;
+        tabla[2] = p3;
 
-        // tabla[0] = p;
-        // tabla[1] = p2;
-        // tabla[2] = p3;
+        System.out.println(Arrays.toString(tabla));
+        Arrays.sort(tabla);
+        System.out.println(Arrays.toString(tabla));
 
-        // System.out.println(Arrays.toString(tabla));
-        // Arrays.sort(tabla);
-        // System.out.println(Arrays.toString(tabla));
+        PorId ordenId = new PorId();
+        Arrays.sort(tabla, ordenId);
+        System.out.println(Arrays.toString(tabla));
+
+        PorNombre ordenNombre = new PorNombre();
+        Arrays.sort(tabla, ordenNombre);
+        System.out.println(Arrays.toString(tabla));
+
     }
 
 }
@@ -65,4 +75,25 @@ class Persona implements Comparable {
         // return this.nombre.compareTo(p.nombre);
         return p.id - this.id;
     }
+}
+
+@SuppressWarnings("rawtypes")
+class PorId implements Comparator {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+
+        return ((Persona) o1).id - ((Persona) o2).id;
+    }
+
+}
+
+class PorNombre implements Comparator {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+
+        return ((Persona) o1).nombre.compareTo(((Persona) o2).nombre);
+    }
+
 }
