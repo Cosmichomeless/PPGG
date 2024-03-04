9da3df79525546692a95b5e4696ee566008ccec0
cosmichomeless <Drodriguez1703@gmail.com>
2024-03-04 13:11:04 +0100

    ASD

--- Java/U11_Acceso_Datos/num.java
+++ Java/U11_Acceso_Datos/num.java
@@ -1,33 +1,43 @@
 package Java.U11_Acceso_Datos;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class num {
     public static void main(String[] args) throws IOException {
+
         BufferedReader reader = new BufferedReader(new FileReader("Java//U11_Acceso_Datos//numeros.txt"));
-        List<Double> numeros = new ArrayList<>();
+        ArrayList<Double> numeros = new ArrayList<>();
         String linea;
 
         while ((linea = reader.readLine()) != null) {
             String[] partes = linea.split(",");
+
             for (String parte : partes) {
                 double num = Double.parseDouble(parte);
                 numeros.add(num);
             }
         }
         reader.close();
-
+        double suma = 0;
+        double media = 0;
         double maximo = Collections.max(numeros);
         double minimo = Collections.min(numeros);
-        double media = 0;
+
         for (double num : numeros) {
             media += num;
         }
+        for (double num : numeros) {
+            suma += num;
+        }
         media /= numeros.size();
-
+        
         System.out.println("Valor máximo: " + maximo);
         System.out.println("Valor mínimo: " + minimo);
+        System.out.println("Suma: " + suma);
         System.out.println("Media: " + media);
     }
 }
