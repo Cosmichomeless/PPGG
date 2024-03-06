acd885f99268b12b4ceb9529549debdb26abcdda
cosmichomeless <Drodriguez1703@gmail.com>
2024-03-06 08:30:21 +0100

    nn

---
+++ Java/U11_Acceso_Datos/Writter.java
@@ -0,0 +1,32 @@
+package Java.U11_Acceso_Datos;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Writter {
+    public static void main(String[] args) {
+        BufferedWriter wr = null;
+        try {
+            wr = new BufferedWriter(new FileWriter("Java//U11_Acceso_Datos//j.txt"));
+
+            String linea = "Vamos a realizar una prueba de escritura en un archivo";
+
+            for (int i = 0; i < linea.length(); i++) {
+                wr.write(linea.charAt(i));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                wr.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Archivo escrito");
+
+    }
+
+}
