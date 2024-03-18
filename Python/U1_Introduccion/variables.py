58e322ea06f09e387203a75280044c592fbd13a8
cosmichomeless <Drodriguez1703@gmail.com>
2024-03-18 12:43:18 +0100

    nueva

--- Python/U1_Introduccion/variables.py
+++ Python/U1_Introduccion/variables.py
@@ -7,3 +7,22 @@ b = 5
 
 print("pepe", a, b)
 
+# esto es un commentary
+x = 1
+y = 2
+z = x + y
+
+print(z)
+
+t = True
+cad1 = "cad1"
+cad2 = " hola "
+cad4 = 25
+
+cad3 = cad1 + cad2 + str(cad4)
+print(cad3)
+
+nombre = input("Dime tu nombre")
+print(nombre)
+
+print(f'hola {nombre}')
